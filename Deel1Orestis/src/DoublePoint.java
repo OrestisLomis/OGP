d6171ec82a9729b866e576007623a5ed347254f2
Orestis Lomis <orestis.lomis@student.kuleuven.be>
2020-02-28 18:12:47 +0100

    Hier zijn mijn bestandjes

---
+++ Deel1Orestis/src/DoublePoint.java
@@ -0,0 +1,17 @@
+
+public class DoublePoint {
+	
+	private double x;
+	private double y;
+	public DoublePoint(double x, double y) {
+		
+	}
+	
+	public double getX() {return this.x;}
+	public double getY() {return this.y;}
+	
+	
+	
+	
+
+}
