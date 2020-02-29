daed4f16b659d293278b873901d7b2547715ae3b
Orestis Lomis <orestis.lomis@student.kuleuven.be>
2020-02-29 23:30:52 +0100

    update

--- OGP_Taak/src/IntVector.java
+++ OGP_Taak/src/IntVector.java
@@ -1,25 +1,43 @@
+package drawit;
 
 public class IntVector {
-
+	
+	private int x;
+	private int y;
+	
 	public IntVector(int x, int y) {
-		// TODO Auto-generated constructor stub
+		this.x = x;
+		this.y = y;
 	}
-
-	public int crossProduct(IntVector other) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
+	
 	public int getX() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.x;
 	}
-
+	
 	public int getY() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.y;
 	}
 	
+	public long crossProduct(IntVector other) {
+		long a = this.getX() * other.getY();
+		long b = this.getY() * other.getX();
+		return a - b;
+	}
 	
+	public boolean isCollinearWith(IntVector other) {
+		return this.crossProduct(other) == 0;
+	}
+	
+	public long dotProduct(IntVector other) {
+		long a = this.getX() * other.getX();
+		long b = this.getY() * other.getY();
+		return a + b;
+	}
+	
+	public DoubleVector asDoublevector() {
+		double a = this.getX();
+		double b = this.getY();
+		return DoubleVector.DoubleVector(a,b);
+	}
 
 }
