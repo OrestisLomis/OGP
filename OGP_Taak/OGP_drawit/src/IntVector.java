e754e550a62a6b4533122b2c15c171c94f596330
Saralismont <61322857+Saralismont@users.noreply.github.com>
2020-03-05 17:08:51 +0100

    Add files via upload
    Met documentatie

--- OGP_Taak/OGP_drawit/src/IntVector.java
+++ OGP_Taak/OGP_drawit/src/IntVector.java
@@ -1,21 +1,50 @@
+package drawit;
 
 public class IntVector {
-
+	/**
+	 * An instance of this class represents a displacement in the two-dimensional plane.
+	 * 
+	 * @immutable
+	 */
 	private int x;
 	private int y;
+	
+	/**
+	 * Initializes the point with the given integers as coordinates
+	 */
 	
 	public IntVector(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
 	
+	/**
+	 * Returns this point's X coordinate.
+	 * 
+	 * @post This point's X coordinate is the given x.
+	 * 		| getX() == x
+	 */
+	
 	public int getX() {
 		return this.x;
 	}
 	
+	/**
+	 * Returns this point's Y coordinate.
+	 * 
+	 * @post This point's Y coordinate is the given y.
+	 */
+	
 	public int getY() {
 		return this.y;
 	}
+	
+	/**
+	 * Returns the cross product of this vector and the given vector.
+	 * 
+	 * @post
+	 * 		| result == (long)getX() * other.getY() - (long)getY() * other.getX()
+	 */
 	
 	public long crossProduct(IntVector other) {
 		long a = this.getX() * other.getY();
@@ -23,9 +52,23 @@ public class IntVector {
 		return a - b;
 	}
 	
+	/**
+	 * Returns whether this vector is collinear with the given vector.
+	 * 
+	 * @post The result is whether the cross product of the two vectors equals zero.
+	 * 		| result == (this.crossProduct(other) == 0)
+	 */
+	
 	public boolean isCollinearWith(IntVector other) {
 		return this.crossProduct(other) == 0;
 	}
+	
+	/**
+	 * Returns the dot product of this vector and the given vector.
+	 * 
+	 * @post The result is the dot product of the two vectors.
+	 * 		| result == (long)getX() * other.getX() + (long)getY() * other.getY()
+	 */
 	
 	public long dotProduct(IntVector other) {
 		long a = this.getX() * other.getX();
@@ -33,10 +76,14 @@ public class IntVector {
 		return a + b;
 	}
 	
+	/**
+	 * Returns a DoubleVector object that represents the same vector represented by this IntVector object. 
+	 */
+	
 	public DoubleVector asDoublevector() {
 		double a = this.getX();
 		double b = this.getY();
-		return new DoubleVector(a, b);
+		return new DoubleVector(a,b);
 	}
 
 }
