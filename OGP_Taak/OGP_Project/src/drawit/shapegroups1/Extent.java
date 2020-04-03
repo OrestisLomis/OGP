cf9a0ddc6199a159a1bcb57041a684eae254890c
OrestisLomis <55985866+OrestisLomis@users.noreply.github.com>
2020-04-03 20:40:33 +0200

    Update Extent.java

--- OGP_Taak/OGP_Project/src/drawit/shapegroups1/Extent.java
+++ OGP_Taak/OGP_Project/src/drawit/shapegroups1/Extent.java
@@ -1,91 +1,144 @@
-package drawit.shapegroups1;
+package drawit.shapegroups2;
 
 import drawit.IntPoint;
 
+/**
+ * Each instance of this class represents a nonempty rectangular area in a 2D coordinate system, 
+ * whose edges are parallel to the coordinate axes. This class must deal with illegal arguments defensively.
+ */
 public class Extent {
 	
 	private int left;
 	private int top;
-	private int right;
-	private int bottom;
+	private int width;
+	private int height;
 	
+	/**
+	 * Returns the X coordinate of the edge parallel to the Y axis with the smallest X coordinate.
+	 */
 	public int getLeft() {
 		return left;
 	}
 	
+	/**
+	 * Returns the Y coordinate of the edge parallel to the X axis with the smallest Y coordinate.
+	 */
 	public int getTop() {
 		return top;
 	}
-	
-	public int getRight() {
-		return right;
-	}
-	
-	public int getBottom() {
-		return bottom;
-	}
-	
+
+	/**
+	 * Returns the distance between the edges that are parallel to the Y axis.
+	 */
 	public int getWidth() {
-		return getRight() - getLeft();
+		return width;
 	}
-	
+
+	/**
+	 * Returns the distance between the edges that are parallel to the X axis.
+	 */
 	public int getHeight() {
-		return getBottom() - getTop();
+		return height;
 	}
 	
+	/**
+	 * Returns the X coordinate of the edge parallel to the Y axis with the largest X coordinate.
+	 */
+	public int getRight() {
+		return getLeft() + getWidth();
+	}
+	
+	/**
+	 * Returns the Y coordinate of the edge parallel to the X axis with the largest Y coordinate.
+	 */
+	public int getBottom() {
+		return getTop() + getHeight();
+	}
+	
+	/**
+	 * Returns the point with the smallest X and Y coordinate.
+	 */
 	public IntPoint getTopLeft() {
 		return new IntPoint(getLeft(), getTop());
 	}
 	
+	/**
+	 * Returns the point with the largest X and Y coordinate.
+	 */
 	public IntPoint getBottomRight() {
 		return new IntPoint(getRight(), getBottom());
 	}
-	
+
+	/**
+	 * Returns whether this extent, considered as a closed set of points (i.e. including its edges and its vertices), contains the given point.
+	 */
 	public boolean contains(IntPoint point) {
-		return (this.getLeft() <= point.getX() && point.getX() <= this.getRight() &&
-				this.getBottom() >= point.getY() && point.getY() >= this.getTop());
+		return point.getX() >= getLeft() && point.getX() <= getRight() && point.getY() >= getTop() && point.getY() <= getBottom();
 	}
 	
+	/**
+	 * Returns an extent with the given variables.
+	 */
 	public static Extent ofLeftTopWidthHeight(int left, int top, int width, int height) {
 		Extent myExtent = new Extent();
 		myExtent.left = left;
 		myExtent.top = top;
-		myExtent.right = left + width;
-		myExtent.bottom = top + height;
-		return myExtent;		
+		myExtent.width = width;
+		myExtent.height = height;
+		return myExtent;
 	}
 	
+	/**
+	 * Returns an extent with the given variables.
+	 */
 	public static Extent ofLeftTopRightBottom(int left, int top, int right, int bottom) {
 		Extent myExtent = new Extent();
 		myExtent.left = left;
 		myExtent.top = top;
-		myExtent.right = right;
-		myExtent.bottom = bottom;
+		myExtent.width = right - left;
+		myExtent.height = bottom - top;
 		return myExtent;
 	}
 	
+	/**
+	 * Returns an object that has the given left coordinate and the same right, top, and bottom coordinate as this object.
+	 */
 	public Extent withLeft(int newLeft) {
-		return ofLeftTopRightBottom(newLeft, this.getTop(), this.getRight(), this.getBottom());
+		return ofLeftTopRightBottom(newLeft, getTop(), getRight(), getBottom());
 	}
 	
+	/**
+	 * Returns an object that has the given top coordinate and the same left, right, and bottom coordinate as this object.
+	 */
 	public Extent withTop(int newTop) {
-		return ofLeftTopRightBottom(this.getLeft(), newTop, this.getRight(), this.getBottom());
+		return ofLeftTopRightBottom(getLeft(), newTop, getRight(), getBottom());
 	}
 	
+	/**
+	 * Returns an object that has the given right coordinate and the same left, top, and bottom coordinate as this object.
+	 */
 	public Extent withRight(int newRight) {
-		return ofLeftTopRightBottom(this.getLeft(), this.getTop(), newRight, this.getBottom());
+		return ofLeftTopRightBottom(getLeft(), getTop(), newRight, getBottom());
 	}
-	
+
+	/**
+	 * Returns an object that has the given bottom coordinate and the same left, top, and right coordinate as this object.
+	 */
 	public Extent withBottom(int newBottom) {
-		return ofLeftTopRightBottom(this.getLeft(), this.getTop(), this.getRight(), newBottom);
+		return ofLeftTopRightBottom(getLeft(), getTop(), getRight(), newBottom);
 	}
 	
+	/**
+	 * Returns an object that has the given width and the same left, top, and bottom coordinate as this object.
+	 */
 	public Extent withWidth(int newWidth) {
-		return ofLeftTopRightBottom(this.getLeft(), this.getTop(), this.getLeft() + newWidth, this.getBottom());
+		return ofLeftTopWidthHeight(getLeft(), getTop(), newWidth, getHeight());
 	}
 	
+	/**
+	 * Returns an object that has the given height and the same left, top, and right coordinate as this object.
+	 */
 	public Extent withHeight(int newHeight) {
-		return ofLeftTopRightBottom(this.getLeft(), this.getTop(), this.getRight(), this.getTop() + newHeight);
+		return ofLeftTopWidthHeight(getLeft(), getTop(), getWidth(), newHeight);
 	}
-		
 }
