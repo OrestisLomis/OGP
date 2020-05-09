63e6f6e0c05d677a1ab539aaf60c8bb474a6478d
Saralismont <61322857+Saralismont@users.noreply.github.com>
2020-05-09 15:18:31 +0200

    Add files via upload

---
+++ OGP_Taak/OGP_Project/src/drawit/shapes1/tests/RoundedPolygonShapeTest.java
@@ -0,0 +1,74 @@
+package drawit.shapes1.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+import drawit.*;
+import drawit.shapes1.ControlPoint;
+import drawit.shapes1.ControlPointPolygon;
+import drawit.shapes1.RoundedPolygonShape;
+import drawit.shapegroups1.*;
+
+
+import org.junit.jupiter.api.Test;
+
+class RoundedPolygonShapeTest {
+
+	@Test
+	void test() {
+		
+		RoundedPolygon myPolygon1 = new RoundedPolygon();
+		LeafShapeGroup SubGroup1 = new LeafShapeGroup(myPolygon1);
+		
+		RoundedPolygon myPolygon2 = new RoundedPolygon();		
+		IntPoint[] myVertices = {new IntPoint(0,0), new IntPoint(0,200), new IntPoint(200,200), new IntPoint(200,0)};
+		myPolygon2.setVertices(myVertices);
+		LeafShapeGroup SubGroup2 = new LeafShapeGroup(myPolygon2);
+		
+		ShapeGroup[] subgroups = new ShapeGroup[] {SubGroup1, SubGroup2};
+		ShapeGroup mygroup = new NonleafShapeGroup(subgroups);
+		
+		RoundedPolygonShape shape1 = new RoundedPolygonShape(mygroup, myPolygon1);
+		
+		assert shape1.getParent() == mygroup;
+		assert shape1.getPolygon() == myPolygon1;
+		assert shape1.contains(new IntPoint(-100,100));
+		assert shape1.contains(new IntPoint(0,0));
+		assert shape1.contains(new IntPoint(0,100));
+		assert myPolygon1.getDrawingCommands().equals(shape1.getDrawingCommands());
+		
+		RoundedPolygonShape shape2 = new RoundedPolygonShape(mygroup, myPolygon2);
+		
+		assert shape2.getParent() == mygroup;
+		assert shape2.getPolygon() == myPolygon2;
+		assert shape2.contains(new IntPoint(0,200));
+		assert shape2.contains(new IntPoint(100,100));
+		assert shape2.contains(new IntPoint(100,200));
+		assert myPolygon2.getDrawingCommands().equals(shape2.getDrawingCommands());
+		
+		Extent myOriginalExtent = mygroup.getOriginalExtent();
+		Extent newExtent = Extent.ofLeftTopRightBottom(0,100,100,200);
+		mygroup.setExtent(newExtent);
+		
+		assert mygroup.getExtent() == newExtent;
+		assert mygroup.getOriginalExtent() == myOriginalExtent;
+		
+		assert shape1.toGlobalCoordinates(new IntPoint(-100,-100)).equals(new IntPoint(0, 100));
+		assert shape2.toGlobalCoordinates(new IntPoint(200, 200)).equals(new IntPoint(100, 200));
+		assert shape1.toGlobalCoordinates(new IntPoint(50, 50)).equals(new IntPoint(50, 150));
+		assert shape1.toShapeCoordinates(new IntPoint(100,200)).equals(new IntPoint(200,200));
+		assert shape2.toShapeCoordinates(new IntPoint(100,100)).equals(new IntPoint(200,-100));
+		assert shape1.toShapeCoordinates(new IntVector(1,3)).getX() == 3;
+		assert shape2.toShapeCoordinates(new IntVector(1,3)).getY() == 9;
+		
+		
+		RoundedPolygonShape shape3 = new RoundedPolygonShape(null, new RoundedPolygon());
+		
+		assert shape3.toGlobalCoordinates(new IntPoint(0,0)).equals(new IntPoint(0,0));
+		assert shape3.toGlobalCoordinates(new IntPoint(-100,100)).equals(new IntPoint(-100,100));
+		assert shape3.toShapeCoordinates(new IntPoint(100, 100)).equals(new IntPoint(100, 100));
+		assert shape3.toShapeCoordinates(new IntVector(2,5)).getX() == 2;
+		assert shape3.toShapeCoordinates(new IntVector(2,5)).getY() == 5;
+		
+		
+	}
+
+}
