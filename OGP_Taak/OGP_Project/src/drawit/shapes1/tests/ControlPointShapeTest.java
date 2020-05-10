5daeb31acf344a0b6e5091243f8b6478d0e65004
Saralismont <61322857+Saralismont@users.noreply.github.com>
2020-05-10 17:02:51 +0200

    Add files via upload

---
+++ OGP_Taak/OGP_Project/src/drawit/shapes1/tests/ControlPointShapeTest.java
@@ -0,0 +1,54 @@
+package drawit.shapes1.tests;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import drawit.IntPoint;
+import drawit.*;
+import drawit.shapegroups1.*;
+import drawit.shapes1.*;
+
+class ControlPointShapeTest {
+
+	@Test
+	void test() {
+		RoundedPolygon myPolygon1 = new RoundedPolygon();
+		RoundedPolygon myPolygon2 = new RoundedPolygon();		
+		IntPoint[] myVertices = {new IntPoint(0,0), new IntPoint(0,200), new IntPoint(200,200), new IntPoint(200,0)};
+		myPolygon2.setVertices(myVertices);
+
+		LeafShapeGroup SubGroup1 = new LeafShapeGroup(myPolygon1);
+		LeafShapeGroup SubGroup2 = new LeafShapeGroup(myPolygon2);
+		
+		ShapeGroup[] subgroups = new ShapeGroup[] {SubGroup1, SubGroup2};
+		ShapeGroup mygroup = new NonleafShapeGroup(subgroups);
+		
+		ShapeGroupShape myshape = new ShapeGroupShape(mygroup);
+		
+		ControlPointShape cpoint1 = new ControlPointShape(myshape, true);
+		ControlPointShape cpoint2 = new ControlPointShape(myshape, false);
+		
+		assert cpoint1.getGroup() == myshape;
+		assert cpoint1.getGroup() == myshape;
+		
+		assert cpoint1.getLocation().equals(new IntPoint(-100,-100));
+		assert cpoint2.getLocation().equals(new IntPoint(200,200));
+		
+		cpoint1.move(new IntVector(100,100));
+		
+		assert cpoint1.getGroup().getShapeGroup() == mygroup;
+		assert mygroup.getExtent().getTopLeft().equals(new IntPoint(0,0));
+		assert cpoint1.getLocation().equals(new IntPoint(-100,-100));
+		
+		cpoint2.move(new IntVector(200,200));
+		
+		assert cpoint2.getGroup().getShapeGroup() == mygroup;
+		assert mygroup.getExtent().getBottomRight().equals(new IntPoint(400,400));
+		assert cpoint2.getLocation().equals(new IntPoint(200,200));
+		
+		assertThrows(UnsupportedOperationException.class, () -> cpoint2.remove());
+		
+	}
+
+}
