08526f28ea8f1a3d53700787c9057c2f0f83615a
Saralismont <61322857+Saralismont@users.noreply.github.com>
2020-05-13 10:33:59 +0200

    Add files via upload

---
+++ OGP_Taak/OGP_Project/src/drawit/ContainsTestStrategyTests/FastRoundedPolygonContainsTestStategyTest.java
@@ -0,0 +1,30 @@
+package drawit.ContainsTestStrategyTests;
+
+import org.junit.jupiter.api.Test;
+
+import drawit.*;
+
+class FastRoundedPolygonContainsTestStategyTest {
+
+	@Test
+	void test() {
+		RoundedPolygon mypolygon = new RoundedPolygon();
+		
+		IntPoint point1 = new IntPoint(200,200);
+		IntPoint point2 = new IntPoint(100, -100);
+		IntPoint point3 = new IntPoint(-100,100);
+		IntPoint point4 = new IntPoint(-50,-50);
+		
+		IntPoint[] points = new IntPoint[] {point1, point2, point4, point3};
+		
+		mypolygon.setVertices(points);
+		
+		assert FastRoundedPolygonContainsTestStrategy.contains(mypolygon, point1);
+		assert FastRoundedPolygonContainsTestStrategy.contains(mypolygon, new IntPoint(0,0));
+		assert FastRoundedPolygonContainsTestStrategy.contains(mypolygon, new IntPoint(150,0));
+		assert !(FastRoundedPolygonContainsTestStrategy.contains(mypolygon, new IntPoint(201, 100)));
+		
+		
+	}
+
+}
---
+++ OGP_Taak/OGP_Project/src/drawit/ContainsTestStrategyTests/PreciseRoundedPolygonContainsTestStrategyTest.java
@@ -0,0 +1,32 @@
+package drawit.ContainsTestStrategyTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import drawit.IntPoint;
+import drawit.PreciseRoundedPolygonContainsTestStrategy;
+import drawit.RoundedPolygon;
+
+class PreciseRoundedPolygonContainsTestStrategyTest {
+
+	@Test
+	void test() {
+		RoundedPolygon mypolygon = new RoundedPolygon();
+		
+		IntPoint point1 = new IntPoint(200,200);
+		IntPoint point2 = new IntPoint(100, -100);
+		IntPoint point3 = new IntPoint(-100,100);
+		IntPoint point4 = new IntPoint(-50,-50);
+		
+		IntPoint[] points = new IntPoint[] {point1, point2, point4, point3};
+		
+		mypolygon.setVertices(points);
+		
+		assert PreciseRoundedPolygonContainsTestStrategy.contains(mypolygon, point1);
+		assert PreciseRoundedPolygonContainsTestStrategy.contains(mypolygon, new IntPoint(0,0));
+		assert !(PreciseRoundedPolygonContainsTestStrategy.contains(mypolygon, new IntPoint(150,0)));
+		assert !(PreciseRoundedPolygonContainsTestStrategy.contains(mypolygon, new IntPoint(201, 100)));
+	}
+
+}
